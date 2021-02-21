fe62e21e053ff9157047b45c36aa9ff180135ca1
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-02-21 13:40:13 -0500

    Done

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+/* eslint-disable default-case */
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success'
 
 export class UserForm extends Component {
     state = {
@@ -12,13 +17,64 @@ export class UserForm extends Component {
 
     }
 
+    // Proceed to next step
+
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+    // Go back to prev step
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    // Handle fields change
     
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        const { step } = this.state;
+        const { firstName, lastName, email, occupation
+        , city, bio } = this.state;
+        const values = { firstName, lastName, email, occupation
+            , city, bio };
+        switch(step) {
+            case 1:
+                return (
+                    <FormUserDetails 
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+            case 2: 
+                return (
+                    <FormPersonalDetails 
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                    )
+            case 3:
+                return (
+                    <Confirm
+                    nextStep={this.nextStep}
+                    prevStep={this.prevStep}
+                    values={values}
+                />
+                )
+            case 4 :
+                    return <Success />
+        }
     }
 }
 
