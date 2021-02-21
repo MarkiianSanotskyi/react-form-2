021117fc72bfb0be4f2b2c283cec47430f0a1a89
Markiian Sanotskyi <marksanockiy@gmail.com>
2021-02-21 12:20:24 -0500

    First commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+
+    }
+
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserForm;
